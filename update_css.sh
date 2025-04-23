2178b3ce15eb27b35fb48bac59ca196ea38691ce
eian <eian@antif4.com>
2025-04-22 23:50:06 -0700

    top menu functional!

--- update_css.sh
+++ update_css.sh
@@ -4,8 +4,8 @@ base_dir="./assets/css/webtui"
 source_dir="./node_modules/@webtui"
 
 mkdir -p $base_dir/base
-mkdir -p $base_dir/theme-catppuccin
+mkdir -p $base_dir/catppuccin
 cp -r $source_dir/css/dist/* $base_dir/base
-cp -r $source_dir/theme-catppuccin/dist/* $base_dir/catppuccin
+cp -r $source_dir/theme-catppuccin/dist/* $base_dir/catppuccin/
 
 echo Completed updating css.  
